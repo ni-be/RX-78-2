2164ab5e545fea844a494f35bb5c4ae6094aa586
Nikolas Bertrand <code@nbertrand.dev>
2023-04-17 21:00:20 +0200

    update

--- auto-install.sh
+++ auto-install.sh
@@ -4,22 +4,22 @@
 # eval "$(ssh-agent -s)"
 # ssh-add ~.ssh/....
 
-# dnf copr enable varlad/helix &
-# #dnf config-manager --add-repo https://brave-browser-rpm-beta.s3.brave.com/brave-browser-beta.repo 
-# #rpm --import https://brave-browser-rpm-beta.s3.brave.com/brave-core-nightly.asc 
-# # dnf install 'dnf-command(config-manager)' 
-# # dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-# # dnf install gh
+dnf copr enable varlad/helix &
+dnf config-manager --add-repo https://brave-browser-rpm-beta.s3.brave.com/brave-browser-beta.repo 
+rpm --import https://brave-browser-rpm-beta.s3.brave.com/brave-core-nightly.asc 
+dnf install 'dnf-command(config-manager)' 
+dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+dnf install gh
 
-# dnf -y install gcc cmake llvm clang-tools-extra clingo golang helix rust cargo exa ripgrep gitui fzf zsh texlive foot brave-browser-beta fcitx5-mozc gnome-shell-extension-pop-shell
-# cargo install bartib felix tokei tealdeer rm-improved ouch git-delta ytop
-# cargo install --locked starship bat zellij zoxide hyperfine broot
+dnf -y install gcc cmake llvm clang-tools-extra clingo golang helix rust cargo exa ripgrep gitui fzf zsh texlive foot brave-browser-beta fcitx5-mozc gnome-shell-extension-pop-shell
+cargo install bartib felix tokei tealdeer rm-improved ouch git-delta ytop
+cargo install --locked starship bat zellij zoxide hyperfine broot
 
-# rustup update 
-# rustup component add clippy 
+rustup update 
+rustup component add clippy 
 
-# dnf -y groupinstall 'Development Tools'
-# dnf -y install procps-ng curl file git
+dnf -y groupinstall 'Development Tools'
+dnf -y install procps-ng curl file git
 
 # CREATE DIRECTORIES
 mkdir /home/nibe/SHOSAI
@@ -70,9 +70,12 @@ ln -sf /home/nibe/ZGMF-X20A/felix /home/nibe/.config/felix
 ln -sf /home/nibe/XVX-016/.zsh_history /home/nibe/.zsh_history
 ln -sf /home/nibe/XVX-016/.gitconfig /home/nibe/.gitconfig
 ln -sf /home/nibe/XVX-016/exercism /home/nibe/.config/exercism
+ln -sf /home/nibe/ZGMF-X20A/fcitx5 /home/nibe/.config/fcitx5
+ln -sf /home/nibe/ZGMF-X20A/autostart /home/nibe/.config/autostart
 
 chsh -s $(which zsh)
 
+#INSTALL LINUX BREW
 curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh && .install.sh
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/nibe/.zprofile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"

ae5d81e42b0e7617d8bd2052ccd9ef82d042f1d5
Nikolas Bertrand <code@nbertrand.dev>
2023-07-27 20:55:04 +0200

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -3,23 +3,29 @@
 # ssh-keygen -t ed25519 -C "code@nbertrand.dev"
 # eval "$(ssh-agent -s)"
 # ssh-add ~.ssh/....
+function brewinstall() {
+     #INSTALL LINUX BREW
+     curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh && .install.sh
+     (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/nibe/.zprofile
+     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-dnf copr enable varlad/helix &
-dnf config-manager --add-repo https://brave-browser-rpm-beta.s3.brave.com/brave-browser-beta.repo 
-rpm --import https://brave-browser-rpm-beta.s3.brave.com/brave-core-nightly.asc 
+}
+dnf -y groupinstall 'Development Tools'
 dnf install 'dnf-command(config-manager)' 
 dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-dnf install gh
+dnf -y install procps-ng curl file git gh python3-pip python3-wheel clang-tools-extra lldb golang cargo zsh texlive fcitx5-mozc
 
-dnf -y install python3-pip python3-wheel gcc cmake llvm clang-tools-extra clingo lldb golang nodejs-npm helix rust cargo exa ripgrep gitui fzf zsh texlive foot brave-browser-beta fcitx5-mozc gnome-shell-extension-pop-shell
-cargo install bartib felix tokei tealdeer rm-improved ouch git-delta ytop
-cargo install --locked starship bat zellij zoxide hyperfine broot
+brew tap wez/wezterm-linuxbrew
+brew install clingo gcc cmake llvm node rust zig exa ripgrep gitui fzf tokei tealdeer rm-improved ouch git-delta btop bartib starship bat zoxide zellij broot hyperfine wezterm
+brew install texlab yaml-language-server lua-language-server marksman  
+
+
 
 rustup update 
 rustup component add clippy 
 
-dnf -y groupinstall 'Development Tools'
-dnf -y install procps-ng curl file git
+
+
 
 #DOWNLOAD GIT REPOS A=REPO B=LOCATION C= name
 function zyx() {
@@ -39,13 +45,7 @@ done < symbolic_links.txt
 chsh -s $(which zsh)
 
 
-function brewinstall() {
-     #INSTALL LINUX BREW
-     curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh && .install.sh
-     (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/nibe/.zprofile
-     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-}
 
 function rust-analyzer-install() {
   cd /home/nibe/GIT/rust-analyzer
@@ -56,15 +56,12 @@ function rust-analyzer-install() {
 
 pip install -U 'python-lsp-server[all]'
 pip install cmake-language-server python-lsp-ruff 
-brew install zig texlab yaml-language-server lua-language-server marksman  
+
 cargo install taplo-cli --locked --features lsp
 npm install -g typescript-language-server typescript intelephense svelte-language-server
 npm i -g vscode-langservers-extracted bash-language-server
 
 go install golang.org/x/tools/gopls@latest github.com/go-delve/delve/cmd/dlv@latest
 
-curl https://nim-lang.org/choosenim/init.sh -sSf | sh
-# May require choosenim
-nimble install nimlangserver
 
 cargo install --git https://git@github.com/guntherwullaert/asp-language-server
